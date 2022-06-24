8854578ee1f9a7bf9e7be22f607d7bb77e142677
lokicodes <lokeshsharma.20020219@gmail.com>
2022-06-24 22:38:55 +0530

    basic messaging enabled

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,18 +8,28 @@ const Chat = ({location}) => {
 
   const [name , setName] = useState('');
   const [room , setRoom] = useState('');
+  const [message , setMessage] = useState('');
+  const [messages , setMessages] = useState([]);
 
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() =>{
     const {name , room} = queryString.parse(location.search);
 
-    socket = io(ENDPOINT, { transports : ['websocket'] });
-
+    socket = io(ENDPOINT, {  
+      cors: {
+      origin: "http://localhost:5000",
+      credentials: true
+    },transports : ['websocket'] });
+    
     setName(name);
     setRoom(room);
 
-    socket.emit('join' , {name, room});
+    socket.emit('join' , {name, room} , (error) => {
+      if(error){
+        alert(error);
+      }
+    });
 
     return () => {
       socket.emit('disconnect');
@@ -27,8 +37,34 @@ const Chat = ({location}) => {
     }
   } , [ENDPOINT , location.search]);
   
+  useEffect(() => {
+    socket.on('message' , (message) => {
+      setMessages([...messages , message]);
+    });
+  } , [messages]);
+
+  const sendMessage = (event) =>{
+    event.preventDefault();
+
+    if(message){
+      socket.emit('sendMessage' , message , () => setMessage(''));
+    }
+  }
+
+  console.log(message, messages);
+
   return (
-    <div>Chat krlo</div>
+    <div className='outerContainer'>
+      <div className='container'>
+        <input
+          value={message}        
+          onChange = {(event) => setMessage(event.target.value)}
+          onKeyPress = {event => event.key === 'Enter' ? sendMessage(event) : null}
+         />
+
+      </div>
+
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -3,15 +3,35 @@ const socketio = require('socket.io');
 const http = require('http');
 const router = require("./router");
 const PORT = process.env.PORT || 5000;
+const {addUser, removeUser , getUser , getUsersInRoom}  = require('./user');
+const { isArgumentsObject } = require('util/types');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 io.on("connection", (socket) => {
-    console.log("User is connected !!");
-    socket.on('join' , ({name, room})=>{
-        console.log(name, room);
-    })
+
+    socket.on('join' , ({name, room} , callback)=>{
+       const {error , user} = addUser({id : socket.id , name, room});
+
+       if (error){
+        return callback(error);
+    } 
+    
+       socket.join(user.room);
+       socket.emit('message' , {user : 'admin' , text: `${user.name} , swagat hai aapka gapshap me , apne room ${user.room} me maze karein`});
+       socket.broadcast.to(user.room).emit('message' , {user : 'admin' , text: `${user.name} hamare sath jud chuke hain. Taaliyan ho Jaye!`});
+       
+});
+
+    socket.on('sendMessage' , (message , callback) =>{
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message' , { user : user.name , text : message});
+
+        callback(); 
+    });
+
     socket.on("disconnect", ()=>{
         console.log("User got disconnected ;(");
     });
