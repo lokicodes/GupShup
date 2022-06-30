db869117737b137d81e6d111fbb2408300e11507
lokicodes <lokeshsharma.20020219@gmail.com>
2022-06-30 11:30:08 +0530

    deployment successfull

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({location}) => {
   const [messages , setMessages] = useState([]);
   const [users, setUsers] = useState('');
   
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://lets-do-gupshup.herokuapp.com/';
 
   useEffect(() =>{
     const {name , room} = queryString.parse(location.search);
